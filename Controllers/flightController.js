9c7b1d1e03f18afc9eee915d927021117ae06993
nieuwie <arjan.nieuwenhuizen@gmail.com>
2012-08-10 18:31:14 +0200

    commit

--- Controllers/flightController.js
+++ Controllers/flightController.js
@@ -2,6 +2,8 @@ sap.ui.controller("sap.training.gw100.controller.doFlightInfo",
 	(function()
 	{
 	    var oModel = oFlightModel.getModel();
+	    var oCompModel = oCompanyModel.getModel();
+	    
 		return{
 			getFlightEntityName: function(){ return "Flight"; },
 			getBookingEntityName: function(){ return "Booking"; },
@@ -20,7 +22,23 @@ sap.ui.controller("sap.training.gw100.controller.doFlightInfo",
                         }
                 );
                 return entitySet;
+            },
+            readCompanyEntitySet: function(entitySet,isASynch)
+            {
+                var entitySet;
+                oCompModel.read(entitySet,null,null,isASynch,
+                        function(oData, response)
+                        {
+                            entitySet = oData.results;
+                        },
+                        function(eError)
+                        {
+                            alert('Something went wrong during communication with the backend');
+                        }
+                );
+                return entitySet;
             }
+            
 		};
 	}())
 );
--- Views/treeView.js
+++ Views/treeView.js
@@ -1,6 +1,10 @@
 sap.ui.jsview("sap.training.gw100.view.treeView",
 		{
 			getControllerName: function(){ return "sap.training.gw100.controller.doFlightInfo"; },
+			getLetterIndex: function(letter)
+			{
+			   return 1;  
+			},
 			createContent: function(oController){
 				
 				
@@ -35,14 +39,53 @@ sap.ui.jsview("sap.training.gw100.view.treeView",
 				oTree.setShowHeaderIcons(true);
 				oTree.setShowHorizontalScrollbar(false);
 				
+				oTree.attachSelect( function(e)
+				{
+				   setAirport(e.mParameters.node.sId); 
+				});
+				
 				var oModel = oFlightModel.getModel();
-				var oRootNode = new sap.ui.commons.TreeNode("root", {text:"Airports", expanded: false});
+				//var oRootNode = new sap.ui.commons.TreeNode("root", {text:"Airports", expanded: false});
+				var letterNodes = [];
+				var oRootNode = new sap.ui.commons.TreeNode("root", {text:"Companies", expanded: false});
+				var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+				var companies = oController.readCompanyEntitySet("Companies", false);
+				var contacts = oController.readCompanyEntitySet("Contacts", false);
+				var companyNodes = [];
+				
+                var oCompanyNode;
+				for(var i=0; i<letters.length; i++)
+                {
+                    oTreeNode = new sap.ui.commons.TreeNode(letters[i], {text:letters[i], icon:"./Views/Images/location.png", expanded: false});
+                    letterNodes.push(oTreeNode);
+                    oRootNode.addNode(oTreeNode);
+                }
+				
+				for(var j=0; j<companies.length; j++)
+                {
+                    var name = companies[j].CompanyName;
+                    var firstLetter = name.charAt(0).toUpperCase();
+                    var letterIndex = parseInt(companies[j].NodeIndex);
+                    console.log(letterIndex+" = LetterIndex");
+                    if( letterIndex >= 0 && letterIndex < 26 && name != "")
+                    {
+                        var node = letterNodes[letterIndex];
+                        oCompanyNode = new sap.ui.commons.TreeNode("A"+companies[j].CompanyId, {text:companies[j].CompanyName, icon:"./Views/Images/location.png", expanded: false});
+                        
+                        node.addNode(oCompanyNode);
+                    }
+
+                }
+
 				oTree.addNode(oRootNode);
 				var oTreeNode;
 				var airportNodes = [];
 				
 		         // Call controller to fetch airports
-                var airports = oController.readEntitySet("Airports", false);
+               /* var airports = oController.readEntitySet("Airports", false);
+				
+				
+				//alert(companies.length + " is aantal companies");
 				
 			    for(var i=0; i<airports.length; i++)
 			    {
@@ -82,7 +125,7 @@ sap.ui.jsview("sap.training.gw100.view.treeView",
                     
                     //oModel.update(context,booking);
                 });
-                
+                */
                 var oLayout = new sap.ui.commons.layout.MatrixLayout({
                     id : "mainLayout",
                     layoutFixed : true,
@@ -96,21 +139,39 @@ sap.ui.jsview("sap.training.gw100.view.treeView",
                 cell1.setVAlign(sap.ui.commons.layout.VAlign.Top);
                 cell1.addContent(oTree);
                 
-                var cell2 = new sap.ui.commons.layout.MatrixLayoutCell();
+                var oCell2 = new sap.ui.commons.layout.MatrixLayoutCell();
+                oCell2.setVAlign(sap.ui.commons.layout.VAlign.Top);
+                oCell2.setSeparation(sap.ui.commons.layout.Separation.Small);
+                //Add the map canvas
+                oCell2.addContent(new sap.ui.core.HTML(
+                                        {
+                                                content : "<div id='map_canvas' style='width: 100%; height: 600px;'></div>"
+                                        }));
+                                        
+                
+                oLayout.createRow(cell1,oCell2);                       
+                /*var cell2 = new sap.ui.commons.layout.MatrixLayoutCell();
                 cell2.setVAlign(sap.ui.commons.layout.VAlign.Top);
                 cell2.setSeparation(sap.ui.commons.layout.Separation.Small);
                 cell2.addContent(bookingsView);
                 
-                oLayout.createRow(cell1,cell2);
+                oLayout.createRow(cell1,oCell2);
+                
                 
                 var cell3 = new sap.ui.commons.layout.MatrixLayoutCell();
                 cell3.setVAlign(sap.ui.commons.layout.VAlign.Top);
                 cell3.setSeparation(sap.ui.commons.layout.Separation.Small);
                 cell3.addContent(oBookingDetails)
                 
-                oLayout.createRow(null,cell3);
+                oLayout.createRow(null,oCell2);*/
 				return oLayout;
 				
 			}
 		}
 );
+
+function getLetterIndex(letter)
+{
+    var letterString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return letterString.indexOf(letter);
+}
